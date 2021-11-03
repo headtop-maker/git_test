30298e852b94b59424be79a67adc9f8f5c46879d
VadiuninDA <VadiuninDA@lad24.ru>
2021-11-03 08:52:59 +0300

    feat: новый компонент

---
+++ src/MainContainer/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Text, useColorScheme, View} from "react-native";
+import {Colors} from "react-native/Libraries/NewAppScreen";
+
+const MainContainer = ()=>{
+
+    return (
+        <View style={styles.sectionContainer}>
+            <Text>
+              Main container
+            </Text>
+
+        </View>
+    );
+};
+ export default MainContainer;
